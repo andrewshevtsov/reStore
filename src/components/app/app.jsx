1855261a404fc5118507012a07e1f84dcc3c1e52
andrewshevtsov <andrewshevtsov@inbox.ru>
2020-06-12 15:33:13 +0300

    base template was initialized

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+
+    return (
+        <div className="container app">
+            <h2>This is App Component title</h2>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+
+    return <div className="spinner">this is spinner</div>
+}
+
+export default Spinner
